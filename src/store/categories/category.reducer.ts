2b78e070ee4dad8188ac201b6f61edbcc6359e62
Pavel <artpavel@ukr.net>
2022-06-23 12:06:16 +0300

    remake card and categories on ts

---
+++ src/store/categories/category.reducer.ts
@@ -0,0 +1,34 @@
+import {Category} from './category.types';
+import {fetchCategoriesFailed, fetchCategoriesStart, fetchCategoriesSuccess} from './category.action';
+import {AnyAction} from 'redux';
+
+export type CategoriesState = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null
+}
+
+export const CATEGORY_INITIAL_STATE: CategoriesState = {
+  categories: [],
+  isLoading: false,
+  error: null
+};
+
+export const categoriesReducer = (
+  state = CATEGORY_INITIAL_STATE,
+  action = {} as AnyAction
+): CategoriesState => {
+  if (fetchCategoriesStart.match(action)) {
+    return {...state, isLoading: true};
+  }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return {...state, categories: action.payload, isLoading: false};
+  }
+
+  if (fetchCategoriesFailed.match(action)) {
+    return {...state, error: action.payload, isLoading: false};
+  }
+
+  return state;
+};
